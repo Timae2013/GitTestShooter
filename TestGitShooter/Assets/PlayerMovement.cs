d3606e9cd3f8d54553d44ce83aad95a2e629455d
Timae2013 <73893713+Timae2013@users.noreply.github.com>
2020-11-03 18:03:13 +0100

    Added Rotation
    Player faces in the mouse Direction

--- TestGitShooter/Assets/PlayerMovement.cs
+++ TestGitShooter/Assets/PlayerMovement.cs
@@ -8,18 +8,26 @@ public class PlayerMovement : MonoBehaviour
     private float moveSpeed = 3f;
 
     public Rigidbody2D player;
+    public Camera cam;
 
     private Vector2 movement;
+    private Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         player.MovePosition(player.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - player.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
+        player.rotation = angle;
     }
 }
