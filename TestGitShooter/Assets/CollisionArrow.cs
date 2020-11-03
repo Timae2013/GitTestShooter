f383ab6d9eeb133df93969bae801269dae522143
Timae2013 <73893713+Timae2013@users.noreply.github.com>
2020-11-03 20:15:29 +0100

    Shooting 1.1
    Löschen des Pfeils beim Verlassen des Bildschirms

---
+++ TestGitShooter/Assets/CollisionArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionArrow : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
